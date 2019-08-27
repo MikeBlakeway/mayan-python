38ecc55067854b57fabfcf0979f8e040764e057c
Mike Blakeway <mike@bluemoonwebsites.co.uk>
2019-08-27 13:35:21 +0100

    integrated slick carousel

--- mayan/urls.py
+++ mayan/urls.py
@@ -1,7 +1,12 @@
 
 from django.urls import path
-from .views import IndexView
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', TemplateView.as_view(template_name="mayan/index.html")),
+    path('about/', TemplateView.as_view(template_name="mayan/about.html")),
+    path('services/', TemplateView.as_view(template_name="mayan/services.html")),
+    path('packages/', TemplateView.as_view(template_name="mayan/packages.html")),
+    path('contact/', TemplateView.as_view(template_name="mayan/contact.html")),
 ]
--- mayan/views.py
+++ mayan/views.py
@@ -4,3 +4,19 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'mayan/index.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'mayan/about.html'
+
+
+class ServicesView(TemplateView):
+    template_name = 'mayan/services.html'
+
+
+class ProductsView(TemplateView):
+    template_name = 'mayan/packages.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'mayan/contact.html'
