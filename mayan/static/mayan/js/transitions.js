b643c77072d705aaeeb1a1f80516aeb832c1b63d
Mike Blakeway <mike@bluemoonwebsites.co.uk>
2019-09-06 13:04:01 +0100

    added navbar functionality and page transition updates

--- mayan/static/mayan/js/transitions.js
+++ mayan/static/mayan/js/transitions.js
@@ -1,16 +1,38 @@
-// Page transitions attached to 
-var button = document.getElementById("left");
+// // Page transitions attached to 
+// var button = document.getElementById("left");
 
-button.addEventListener('click', () => {
-    var url = link;
-    var layerClass = ".layer";
-    var layers = document.querySelectorAll(layerClass);
+// button.addEventListener('click', () => {
+//     var url = link;
+//     // var layerClass = ".layer";
+//     // var layers = document.querySelectorAll(layerClass);
 
-    for (const layer of layers) {
-        layer.classList.toggle("active");
-    };
-    document.getElementById("page").classList.add("fade");
-    setTimeout(function () {
-        document.location.href = url
-    }, 900);
-});
+//     // for (const layer of layers) {
+//     //     layer.classList.toggle("active");
+//     // };
+
+//     if (overlay.isAnimating) {
+//         return false;
+//     }
+//     overlay.toggle();
+//     if (overlay.isOpened === true) {
+//         elmHamburger.classList.add('is-opened-navi');
+//         for (var i = 0; i < gNavItems.length; i++) {
+//             gNavItems[i].classList.add('is-opened');
+//         }
+//     } else {
+//         elmHamburger.classList.remove('is-opened-navi');
+//         for (var i = 0; i < gNavItems.length; i++) {
+//             gNavItems[i].classList.remove('is-opened');
+//         }
+//     }
+
+//     document.getElementById("page").classList.add("fade");
+//     setTimeout(function () {
+//         document.location.href = url
+//     }, 900);
+// });
+
+// // button.addEventListener('click', () => {
+// //     const url = link;
+
+// // });
