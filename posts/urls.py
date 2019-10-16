7e7a6b3a56ee7f49f086d6c5e52456357403a54d
Mike Blakeway <mike@bluemoonwebsites.co.uk>
2019-10-16 12:42:50 +0100

    ready to upload to heroku

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import PostListView, PostDetailView, CategoryView
+from .views import PostListView, PostDetailView, CategoryListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name="post-list"),
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('category/', CategoryView.as_view(), name="category"),
+    path('category/', CategoryListView.as_view(), name="category-list"),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -15,14 +15,13 @@ def get_category_count():
 
 class PostListView(ListView):
     model = Post
-    template_name = "articles/"
     context_object_name = 'posts'
     paginate_by = 5
     queryset = Post.objects.filter(published=True).order_by('-created_date')
     category_count = get_category_count()
 
-    def get_context_data(self):
-        context = super().get_context_data()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         context['category_count'] = self.category_count
         context['authors'] = Author.objects.all()
@@ -32,13 +31,43 @@ class PostListView(ListView):
         return context
 
 
+class CategoryListView(ListView):
+
+    # TODO
+    # * --> add this feature to the page
+    # * --> it should return the posts that are related to the category chosen
+    # * --> each category listed should link to the CategoryDetailView
+    # * --> CategoryDetailView to be added
+
+    model = Category
+    context_object_name = 'categories'
+    queryset = Category.objects.order_by('title')
+    category_count = get_category_count()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['category_count'] = self.category_count
+        context['authors'] = Author.objects.all()
+        context['featured'] = Post.objects.filter(published=True,
+                                                  featured=True).order_by('-created_date')[0:3]
+        return context
+
+
 class PostDetailView(DetailView):
     queryset = Post.objects.all()
+    category_count = get_category_count()
 
     def get_object(self):
         obj = super().get_object()
         return obj
 
-
-class CategoryView(TemplateView):
-    template_name = 'posts/category.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['category_count'] = self.category_count
+        context['authors'] = Author.objects.all()
+        context['featured'] = Post.objects.filter(published=True,
+                                                  featured=True).order_by('-created_date')[0:3]
+        context['latest'] = Post.objects.order_by('-created_date')[0:3]
+        return context
